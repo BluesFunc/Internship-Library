366a8763182793fe3b31c462ff18c1d291424a03
Alex <aleksejmakarov432@gmail.com>
2025-04-01 12:07:56 +0300

    Add application layer  interfaces

---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Abstraction;
+
+namespace Application.Interfaces;
+
+public interface IUnitOfWork: IDisposable
+{
+    IGenericRepository<T> Repository<T>() where T : Entity;
+    Task<int> Save(CancellationToken cancellationToken);
+    Task Rollback();
+}
---
+++ Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Abstraction;
+
+namespace Application.Interfaces.Repositories;
+
+public interface IGenericRepository<T> where T: Entity 
+{
+    IQueryable<T> Entities { get; }
+    Task<T> GetByIdAsync(Guid id);
+    Task<List<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}

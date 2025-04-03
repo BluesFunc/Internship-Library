b5cd22381f46551a4d8947d344b10bc44df7389c
Alex <aleksejmakarov432@gmail.com>
2025-04-03 18:47:45 +0300

    Create DTOs

---
+++ Application/DTOs/_Account_/AuthenticationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTOs._Account_;
+
+public class AuthenticationRequest
+{
+    public string Mail { get; set; }
+    public string Password { get; set; }
+}
---
+++ Application/DTOs/_Book_/BookDto.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Mapster;
+
+namespace Application.DTOs._Book_;
+
+public class BookDto : IMapFrom<Book>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public BookGenre Genre { get; set; }
+    public string Description { get; set; } = null!;
+    public bool IsBooked { get; set; }
+    public string Image { get; set; } = null!;
+    public Author Author { get; set; } = null!;
+    
+    public void ConfigureMapping(TypeAdapterConfig config)
+    {
+        config.NewConfig<Book, BookDto>()
+            .Map(dest => dest.IsBooked, src => src.BookedBy != null)
+            .RequireDestinationMemberSource(true);
+    }
+}

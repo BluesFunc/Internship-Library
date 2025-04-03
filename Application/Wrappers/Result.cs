1cf994650bed35e1bdd807e5ca21015da36da5c3
Alex <aleksejmakarov432@gmail.com>
2025-04-03 18:47:22 +0300

    Create application wrappers

---
+++ Application/Wrappers/Result.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Application.Wrappers;
+
+public record Result
+{
+    public bool Succeed { get; init; }
+    
+    public static Result Successful() => new Result() { Succeed = true };
+    
+    public static Result Failed() => new Result() { Succeed = false};
+
+}
+
+public record Result<T> : Result
+{
+    public T Content { get; init; } = default!;
+
+    public static Result<T> Successful(T content)
+        => new Result<T>()
+        {
+            Succeed = true,
+            Content = content
+        };
+}
